af4d5413c10fec1d5ac3f33cb890e858beed5dd7
Marcelo Felix <124020683+7kfelix@users.noreply.github.com>
2025-05-30 12:26:31 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,10 +4,10 @@ import os
 
 SECRET_KEY = '1Qrfgbdf21A6a1dddf6d1'
 
-DB_USER = 'root'
-DB_PASSWORD = '10451236aA%40' # Coloque sua senha correta
-DB_SERVER = '127.0.0.1'       # <<<< IMPORTANTE: Verifique esta linha!
-DB_PORT = 3306
+DB_USER = 'SEU-USUARIO'
+DB_PASSWORD = 'SUA-SENHA' # Coloque sua senha correta
+DB_SERVER = 'IP-DO-SEU-BANCO'       # <<<< IMPORTANTE: Verifique esta linha!
+DB_PORT = PORTA-DO-SEU-BANCO
 DB_NAME = 'jogoteca'
 DB_DIALECT = 'mysql+mysqlconnector'
 
